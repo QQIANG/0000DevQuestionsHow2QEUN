ad902896854dd26369b3cbb581b12a719b159bfa
JNYJ(William.zhou) <qqiang.dev@gmail.com>
2014-11-14 13:54:20 +0800

    add Block (create, call)

---
+++ 0000DevQuestionsHow2QEUN/0000DevQuestionsHow2QEUN/BlockTesting.h
@@ -0,0 +1,13 @@
+//
+//  BlockTesting.h
+//  DevQuestionsHow2QEUN
+//
+//  Created by JNYJ on 14-11-14.
+//  Copyright (c) 2014年 JNYJ. All rights reserved.
+//
+
+#import "DevQuestionsHow2QEUN-swift.h"
+
+@interface BlockTesting : NSObject
+
+@end
